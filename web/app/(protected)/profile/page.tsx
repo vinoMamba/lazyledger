ca62ce623138a1f3b112d086f6324f91fd2a0749
vino <vino0908@outlook.com>
2025-01-03 10:47:19 +0800

    profile layout

--- web/app/(protected)/profile/page.tsx
+++ web/app/(protected)/profile/page.tsx
@@ -1,54 +1,27 @@
-import { getUserInfoAction } from "@/actions/get-user-info";
-import { PasswordDialog } from "@/components/profile/password-dialog";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { getFirstStr } from "@/lib/string";
-import { AvatarUpload } from "@/components/profile/avatar-upload";
-import { EmailDialog } from "@/components/profile/email-dialog";
-import { UsernameDialog } from "@/components/profile/username-dialog";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ProfileCard } from "@/components/profile/profile-card";
 
-export default async function ProfilePage() {
-  const userInfo = await getUserInfoAction();
-  const url = `${process.env.NEXT_API_URL}/upload/icon/${userInfo?.avatar}`
+export default function ProfilePage() {
   return (
-    <Card className="m-14 border-none shadow-none">
-      <CardHeader>
-        <CardTitle className=" text-3xl">个人设置</CardTitle>
-        <CardDescription></CardDescription>
-      </CardHeader>
-      <CardContent className="max-w-screen-md space-y-4">
-        <div className="flex items-end gap-8">
-          <Avatar className="w-20 h-20">
-            <AvatarImage src={url} alt={userInfo?.username} />
-            <AvatarFallback className="text-muted-foreground">{getFirstStr(userInfo?.username)}</AvatarFallback>
-          </Avatar>
-          <AvatarUpload />
-        </div>
-        <div className="flex flex-col gap-4 text-sm">
-          <span className="text-muted-foreground text-lg font-semibold">用户名</span>
-          <div className=" flex items-center gap-4">
-            <Input value={userInfo?.username} disabled />
-            <UsernameDialog username={userInfo?.username} />
-          </div>
-        </div>
-        <div className="space-y-4">
-          <div className="flex flex-col gap-4 text-sm">
-            <span className="text-muted-foreground text-lg font-semibold">邮箱</span>
-            <div className=" flex items-center gap-4">
-              <Input value={userInfo?.email} disabled />
-              <EmailDialog email={userInfo?.email} />
-            </div>
-          </div>
-          <div className="flex flex-col gap-4 text-sm">
-            <span className="text-muted-foreground text-lg font-semibold">密码</span>
-            <div className=" flex items-center gap-4">
-              <Input value="********************************" type="password" disabled />
-              <PasswordDialog />
-            </div>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+    <Tabs defaultValue="profile" className="m-14">
+      <TabsList>
+        <TabsTrigger value="profile" className="px-8">个人设置</TabsTrigger>
+        <TabsTrigger value="category" className="px-8">分类设置</TabsTrigger>
+        <TabsTrigger value="tag" className="px-8">标签设置</TabsTrigger>
+        <TabsTrigger value="model" className="px-8">大模型设置</TabsTrigger>
+      </TabsList>
+      <TabsContent value="profile" className="space-y-4">
+        <ProfileCard />
+      </TabsContent>
+      <TabsContent value="category">
+        <div>分类设置</div>
+      </TabsContent>
+      <TabsContent value="tag">
+        <div>标签设置</div>
+      </TabsContent>
+      <TabsContent value="model">
+        <div>大模型设置</div>
+      </TabsContent>
+    </Tabs>
   )
 }
---
+++ web/components/profile/profile-card.tsx
@@ -0,0 +1,51 @@
+import { getUserInfoAction } from "@/actions/get-user-info";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { getFirstStr } from "@/lib/string";
+import { AvatarUpload } from "@/components/profile/avatar-upload";
+import { EmailDialog } from "@/components/profile/email-dialog";
+import { UsernameDialog } from "@/components/profile/username-dialog";
+import { PasswordDialog } from "@/components/profile/password-dialog";
+
+export const ProfileCard = async () => {
+  const userInfo = await getUserInfoAction();
+  const url = `${process.env.NEXT_API_URL}/upload/icon/${userInfo?.avatar}`
+  return (
+    <>
+      <div className="border flex flex-col gap-4 p-8 max-w-screen-md rounded-lg">
+        <div className="flex items-end  gap-8">
+          <Avatar className="w-20 h-20">
+            <AvatarImage src={url} alt={userInfo?.username} />
+            <AvatarFallback className="text-muted-foreground">{getFirstStr(userInfo?.username)}</AvatarFallback>
+          </Avatar>
+          <AvatarUpload />
+        </div>
+        <div className="flex flex-col gap-4 text-sm">
+          <span className="text-muted-foreground text-lg font-semibold">用户名</span>
+          <div className=" flex items-center gap-4">
+            <Input value={userInfo?.username} disabled className="disabled:opacity-100" />
+            <UsernameDialog username={userInfo?.username} />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 p-8 max-w-screen-md rounded-lg border">
+        <div className="space-y-4">
+          <div className="flex flex-col gap-4 text-sm">
+            <span className="text-muted-foreground text-lg font-semibold">邮箱</span>
+            <div className=" flex items-center gap-4">
+              <Input value={userInfo?.email} disabled className="disabled:opacity-100" />
+              <EmailDialog email={userInfo?.email} />
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 text-sm">
+            <span className="text-muted-foreground text-lg font-semibold">密码</span>
+            <div className=" flex items-center gap-4">
+              <Input value="********************************" type="password" disabled className="disabled:opacity-100" />
+              <PasswordDialog />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
