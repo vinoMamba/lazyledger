be6ac5b73bbf528414a6f39fd9b2d8f89a1acdf1
vino <vino0908@outlook.com>
2025-01-14 16:57:17 +0800

    update transaction panel

--- web/components/transaction/transaction-panel.tsx
+++ web/components/transaction/transaction-panel.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { useTransaction } from "@/hooks/use-transaction"
-import { CategoryIcon } from "../category/catetory-icon"
 import { Separator } from "../ui/separator"
-import { format } from "date-fns"
+import { cn } from "@/lib/utils"
+import { CategoryCell } from "../category/category-cell"
 
 export const TransactionPanel = () => {
   const currentTransaction = useTransaction(s => s.currentTransaction)
@@ -11,13 +11,15 @@ export const TransactionPanel = () => {
   if (!currentTransaction) return null
 
   return (
-    <div className="flex flex-col gap-4 p-4">
-      <h6 className="text-lg font-medium">{currentTransaction?.name}</h6>
-      <span className="text-sm text-muted-foreground">{format(currentTransaction?.date, 'yyyy-MM-dd')}</span>
+    <div className="flex flex-col gap-4 p-4 cursor-default">
+      <h6 className="text-2xl font-bold">{currentTransaction.name}</h6>
+      <div className="flex items-start justify-between pr-4">
+        <span className={cn(currentTransaction.type === 1 && " text-green-700", "text-2xl font-semibold")}>{currentTransaction.amount}</span>
+        <span className="text-sm text-muted-foreground">{currentTransaction.date}</span>
+      </div>
       <div className="flex items-center gap-2">
-
+        <CategoryCell value={currentTransaction.categoryId} />
         {/* <p className="text-xl font-bold text-muted-foreground" style={{ color: currentTransaction?.type === 'income' ? 'green' : 'red' }}>¥{currentTransaction?.amount}</p> */}
-        <CategoryIcon category={currentTransaction?.categoryId || null} />
       </div>
       {/* <div className="flex items-center gap-2">
         {currentTransaction?.tags.map(tag => (
